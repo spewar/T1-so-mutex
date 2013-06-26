83a6856ae6827170e0b5c263cb946c0266ce200e
marcelo <marcelo@marcelo-RV411-RV511-E3511-S3511-RV711-E3411>
2013-06-26 16:10:27 -0300

    Primeiro commit para teste, inserindo o projeto do netbeans juntamente com o mutex.h

---
+++ mutex.h
@@ -0,0 +1,23 @@
+/* 
+ * File:   mutex.h
+ * Author: marcelo
+ *
+ * Created on 26 de Junho de 2013, 16:08
+ */
+
+#ifndef MUTEX_H
+#define	MUTEX_H
+
+#ifdef	__cplusplus
+extern "C" {
+#endif
+
+
+
+
+#ifdef	__cplusplus
+}
+#endif
+
+#endif	/* MUTEX_H */
+
---
+++ prodcons.c
@@ -0,0 +1,18 @@
+/* 
+ * File:   prodcons.c
+ * Author: marcelo
+ *
+ * Created on 26 de Junho de 2013, 16:07
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+/*
+ * 
+ */
+int main(int argc, char** argv) {
+
+    return (EXIT_SUCCESS);
+}
+
