f1641a6fd2e58555342f580ea985fe71cd00d3c7
marcelo <marcelo@marcelo-RV411-RV511-E3511-S3511-RV711-E3411>
2013-07-07 16:34:39 -0300

    Modifiquei a biblioteca mutex e criei 2 funções na main para testar o mutex, a soma e a subtrai. Aparentemente agora está funcionando o mutex.

--- mutex.h
+++ mutex.h
@@ -5,15 +5,20 @@
  * Created on 26 de Junho de 2013, 16:08
  */
 #include <opa_primitives.h>
+OPA_int_t y;
 
-void lock (OPA_int_t *y){
-    while(1){
-                if(OPA_cas_int(y,1,0)) {
-			break;
-		}
+void inic() {
+    OPA_store_int(&y, 1);
+}
+
+void lock() {
+    while (1) {
+        if (OPA_cas_int(&y, 1, 0)) {
+            break;
+        }
     }
 }
 
-void unlock (OPA_int_t *y){
-    OPA_store_int(y,1);;
+void unlock() {
+    OPA_store_int(&y, 1);
 }
--- prodcons.c
+++ prodcons.c
@@ -7,12 +7,44 @@
 
 #include <stdio.h>
 #include <stdlib.h>
+#include "mutex.h"
+#include <pthread.h>
 
 /*
- * 
+ * Funções soma e subtrai servem para testar o mutex
  */
-int main(int argc, char** argv) {
 
-    return (EXIT_SUCCESS);
+void *soma();
+void *subtrai();
+int valor;
+
+void main() {
+    inic();
+    pthread_t som[50], sub[50];
+    valor = 0;
+    printf("Começo da execução\n");
+    int i;
+    for (i = 0; i < 50; i++) {
+        pthread_create(&som[i], NULL, soma, NULL);
+        pthread_create(&sub[i], NULL, subtrai, NULL);
+    }
+    for (i = 0; i < 50; i++) {
+        pthread_join(som[i], NULL);
+        pthread_join(sub[i], NULL);
+    }
+    printf("valor final eh=""%d\n", valor);
 }
 
+void *soma() {
+    printf("soma\n");
+    lock();
+    valor = valor + 2;
+    unlock();
+}
+
+void *subtrai() {
+    printf("subtrai\n");
+    lock();
+    valor--;
+    unlock();
+}
