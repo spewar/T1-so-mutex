9b558d59fc958c67bf49a40af710d609e868fc0a
juncal <juncal@juncal-ONKYOPC>
2013-07-07 21:38:08 -0300

    Adicionei as funções da fila circular... insere, remove, verificar se esta cheio e se esta vazio.

--- prodcons.c
+++ prodcons.c
@@ -10,6 +10,23 @@
 #include "mutex.h"
 #include <pthread.h>
 
+#define TAM 10
+
+int qtd_char = 0;      // numero de char na fila
+int ultimo_lido = 0;   // posição do ultimo char lido
+int ultimo_inserido = 0;  // posiçao do ultimo char colocado
+
+char buffer[TAM];
+
+/*
+ * Funções da fila
+ */
+
+int buffer_cheio(void);
+int buffer_vazio(void);
+void inserir(char c);
+void remover(void);
+
 /*
  * Funções soma e subtrai servem para testar o mutex
  */
@@ -47,4 +64,40 @@ void *subtrai() {
     lock();
     valor--;
     unlock();
+}
+
+// funções da fila circular
+// coloca o char na fila
+void inserir(char c)
+{
+    // atualiza ultimo elemento, e aumenta quantidade de elementos
+    if (++ultimo_inserido >= TAM){
+        ultimo_inserido = 0;
+    }
+    buffer[ultimo_inserido] = c;
+    printf("inserido o char %c\n", c);
+    qtd_char++;
+}
+
+// retira char da fila
+void remover(void)
+{
+  if (++ultimo_lido >= TAM){
+      ultimo_lido = 0;
+  }
+  printf("retirado o char %c\n", buffer[ultimo_lido]);
+  // limpa o espaço do char removido
+  buffer[ultimo_lido] = 0x20;
+  qtd_char--;
+}
+
+// retorna 1 se o buffer esta cheio e 0 do contrario
+int buffer_cheio(void)
+{
+    return ultimo_lido == ultimo_inserido && qtd_char == TAM;
+}
+// retorna 1 se o buffer esta vazio e 0 do contrario
+int buffer_vazio(void)
+{
+    return ultimo_lido == ultimo_inserido && qtd_char == 0;
 }
